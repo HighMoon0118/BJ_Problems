e90d6e4b5cf74d3c3afd258f77e6a868095f0792
moon <mhi0118@naver.com>
2021-01-24 18:02:24 +0900

    BJ7562

--- BJ7562.py
+++ BJ7562.py
@@ -3,28 +3,37 @@ from collections import deque
 
 input = sys.stdin.readline
 
-c = int(input())
-ans = []
+n, m = map(int, input().split())
+board = []
 
-for _ in range(c) :
-    n = int(input())
-    sR, sC = map(int, input().split())
-    eR, eC = map(int, input().split())
+for i in range(n) :
+    board.append(list(map(int, input().strip())))
+    
+visited = [[0 for _ in range(m)] for _ in range(n)]
 
-    dR = 1, 2, 2, 1, -1, -2, -2, -1
-    dC = 2, 1, -1, -2, -2, -1, 1, 2
-    visited = [[ 0 for _ in range(n)] for _ in range(n)]
-    que = deque()
-    que.append((sR, sC, 0))
-    visited[sR][sC]=1
-    while que :
-        r, c, count = que.popleft()
-        if r==eR and c==eC : break
-        for i in range(8) :
-            nR, nC = r+dR[i], c+dC[i]
-            if 0<=nR<n and 0<=nC<n and not visited[nR][nC] :
-                visited[nR][nC] = 1
-                que.append((nR,nC,count+1))
-    ans.append(count)
+dr = 0, 1, 0, -1
+dc = 1, 0, -1, 0
 
-print('\n'.join(map(str, ans)))
+que = deque()
+que.append((0,0,1,1))
+visited[0][0]=1
+finish = False
+
+while que :
+    r, c, boom, count = que.popleft()
+    if r==n-1 and c==m-1 : 
+        finish=True
+        break
+    for i in range(4) :
+        nr, nc = r+dr[i], c+dc[i]
+        if 0<=nr<n and 0<=nc<m :
+            if board[nr][nc]==0 :
+                if not visited[nr][nc] or visited[nr][nc]>boom :
+                    visited[nr][nc]=boom
+                    que.append((nr, nc, boom, count+1))
+            elif boom==1 :
+                visited[nr][nc]=boom+1
+                que.append((nr, nc, boom+1, count+1))
+
+if finish : print(count)
+else : print(-1)
