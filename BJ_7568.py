ad2f08ee7f1420024f161fc8d2b113656de82ba3
moon <mhi0118@naver.com>
2021-01-28 17:45:53 +0900

    BJ7568

--- BJ_7568.py
+++ BJ_7568.py
@@ -6,15 +6,15 @@ n = int(input())
 info = []
 for i in range(n) :
     w, t = map(int, input().split())
-    info.append((i, w, t))
+    info.append((w, t, i))
 
-info.sort(key=lambda x : x[1])
+info.sort()
 ans = [1 for _ in range(n)]
 for i in range(n) :
     count = 0
     for j in range(i+1, n) :
-        if info[i][2]<info[j][2] :
+        if info[i][0]<info[j][0] and info[i][1]<info[j][1] :
             count+=1
-    ans[info[i][0]]+=count
+    ans[info[i][2]]+=count
 
 print(*ans)
