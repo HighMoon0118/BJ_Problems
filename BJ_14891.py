30abe485f3327b9ee135f79eaea64b02cace7714
moon <mhi0118@naver.com>
2021-02-04 17:35:09 +0900

    bj14891

--- BJ_14891.py
+++ BJ_14891.py
@@ -25,21 +25,21 @@ for _ in range(n) :
     rotate = [0 for _ in range(4)]  # 시계로 회전하는지 반시계로 회전하는지
     rotate[num-1] = how
 
-    for i in range(3) :
+    for i in range(3) :  # 현재 톱니 상태 저장
         if check_right(i)==check_left(i+1) :
             status[i]=1
         else : status[i]=-1
 
     left = right = num-1
     l_how = r_how = how
-    while left-1>=0 :
+    while left>0 :  # 기준 톱니 왼쪽 체크
         left-=1
         if status[left]==-1 :
             rotate[left]=-1*l_how
             l_how = rotate[left]
         elif status[left]==1 :
             l_how = 0
-    while right+1<4 :
+    while right<3 : # 기준 톱니 오른쪽 체크
         right+=1
         if status[right-1]==-1 :
             rotate[right]=-1*r_how
@@ -47,7 +47,7 @@ for _ in range(n) :
         elif status[right-1]==1 :
             r_how = 0
 
-    for i in range(4) :
+    for i in range(4) :  # 회전한 톱니바퀴에서 12시에 위치한 index 저장
         if rotate[i]==1 :
             start[i]+=7
         elif rotate[i]==-1 :
