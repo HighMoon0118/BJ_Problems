6ec008b0ae624ac55b146dc2a12caedacd12c752
moon <mhi0118@naver.com>
2021-02-15 10:34:19 +0900

    study

--- 스터디/BJ_1138.py
+++ 스터디/BJ_1138.py
@@ -9,7 +9,7 @@ order = list(map(int, input().split()))
 line = []  # 줄
 num = n
 
-for o in order[::-1]:
+for o in order[::-1]:  # 가장 키가 큰 사람부터 시작, 본인보다 큰 사람이 있는 만큼 오른쪽으로 이동해서 삽입
     line.insert(o, num)
     num-=1
 
