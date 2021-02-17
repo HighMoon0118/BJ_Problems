41adae7191a9e2ef021853c3a0759319e5e51844
moon <mhi0118@naver.com>
2021-02-17 17:54:27 +0900

    study

--- 스터디/BJ_17413.py
+++ 스터디/BJ_17413.py
@@ -1,26 +1,26 @@
 word = input()
 
 is_tag = False
-ans = ""
-sub = ""
+ans = ""  # 최종적으로 만들 String
+sub = ""  # 뒤집을 문자를 담을 String
 
 for c in word:
     if c=="<" or c==">":
-        if c=="<":
-            ans += sub[::-1]
+        if c=="<":  # 태그가 시작됐을 때
+            ans += sub[::-1]  # 이전에 단어를 다뤘다면 저장
             sub = ""
             is_tag = True
-        else:
+        else:  # 태그가 끝났을 때
             is_tag = False
         ans += c
         continue
     
-    if is_tag:
+    if is_tag:  # 현재 태그를 다룰 때
         ans += c
-    elif c==" ":
+    elif c==" ":  # 한 단어가 끝났을 때
         ans += sub[::-1] + c
         sub = ""
-    else:
+    else:  # 태그가 아닌 문자일 때
         sub += c
 ans += sub[::-1]
 print(ans)
