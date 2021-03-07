70ebefe1e4674b0367421142d1cc89eb883fb9b2
moon <mhi0118@naver.com>
2021-03-07 21:09:59 +0900

    bj12101

---
+++ 스터디/BJ_12101.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+def makeIt(num, result):
+    global ans, count
+    if count == k or num > n:
+        return
+    if num==n:
+        count += 1
+        if count == k:
+            ans = result
+        return
+    for i in range(1, 4):
+        makeIt(num+i, result+str(i)+"+")
+
+n, k = map(int, input().split())
+ans, count = "-1 ", 0
+makeIt(0, "")
+print(ans[0:len(ans)-1])
