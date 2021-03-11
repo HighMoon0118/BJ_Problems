db54dc0f9bfd15c70c25b69dca4d3cdaf914939f
moon <mhi0118@naver.com>
2021-03-11 22:11:50 +0900

    bj1174

---
+++ 스터디/BJ_1174.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+def makeIt(startN, index):
+    global count, ans
+
+    if startN < 0 or ans: return
+
+    num[index] = startN
+    
+    if index == 0:
+        count += 1
+        if count == n:
+            ans = "".join(map(str, num[:i+1]))
+            finish: True
+        return
+
+    for j in range(0, startN):
+        makeIt(j, index-1)
+
+
+n = int(input())
+num = [0 for _ in range(10)]
+count, ans = 0, ""
+for i in range(10):
+    for j in range(i, 10):
+        makeIt(j, i)
+        if ans: break
+    if ans: break
+
+if ans: print(ans[::-1])
+else: print(-1)
