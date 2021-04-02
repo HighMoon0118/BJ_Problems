aa2c830a2ceadf15e522cf08c127e01db0b0eb2a
moon <mhi0118@naver.com>
2021-04-02 19:32:56 +0900

    study

--- 스터디/BJ_9375.py
+++ 스터디/BJ_9375.py
@@ -4,11 +4,14 @@ tc = int(input())
 ans = []
 
 for _ in range(tc):
+    
     n = int(input())
     diction = {}
+
     for _ in range(n):
         cloth, sort = input().split()
         diction[sort] = diction.get(sort, 1) + 1
+
     result = 1
     for v in diction.values():
         result *= v
