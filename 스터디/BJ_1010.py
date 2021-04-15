1a356c39cb9aa6463d93169be2e9529a6c2e7905
moon <mhi0118@naver.com>
2021-04-16 08:38:34 +0900

    1010

---
+++ 스터디/BJ_1010.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+def make_it(a, b):
+    if b==0 or a==b: return 1
+    if dp[a][b] >=0: return dp[a][b]
+    
+    dp[a][b] = make_it(a-1, b) + make_it(a-1, b-1)
+    return dp[a][b]
+
+t = int(input())
+    
+dp = [[-1 for _ in range(31)] for _ in range(31)]
+ans = []
+for tc in range(1, t+1):
+    n, m = map(int, input().split())
+
+    ans.append(str(make_it(m, n)))
+print("\n".join(ans))
