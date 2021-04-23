769dada34cde8d0ae735322e50f14d74644b5ca8
moon <mhi0118@naver.com>
2021-04-23 15:55:12 +0900

    study

---
+++ 스터디/BJ_11053.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+num = list(map(int, input().split()))
+
+st = [num[0]]
+for i in range(1, n):
+    if st[-1] < num[i]:
+        st += [num[i]]
+    elif st[-1] > num[i]:
+        l, r = 0, len(st)-1
+        while l<r:
+            m = (l+r)//2
+            if st[m] < num[i]:
+                l = m+1
+            else: 
+                r = m
+        st[r] = num[i]
+print(len(st))
