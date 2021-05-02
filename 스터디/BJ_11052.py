a588a116b65a10c9b0989264e03bb6608ec02c8a
moon <mhi0118@naver.com>
2021-05-02 10:32:05 +0900

    study

--- 스터디/BJ_11052.py
+++ 스터디/BJ_11052.py
@@ -1,24 +1,43 @@
 import sys
 sys.setrecursionlimit(100000)
-def make_it(index, cnt):
+def make_it(cnt):
+    if cnt == 0: return 0
 
-    if index > n: return 0
+    if dp[cnt]>=0: return dp[cnt]
 
-    if dp[index][cnt]>=0: return dp[index][cnt]
-
-    result = make_it(index+1, cnt)
-    count = cnt+index
-    plus = cost[index]
-    while count <= n:
-        result = max(result, plus + make_it(index+1, count))
-        count += index
-        plus += cost[index]
-    dp[index][cnt] = result
+    result = 0
+    for i in range(1, cnt+1):
+        result = max(result, cost[i] + make_it(cnt-i))
+    dp[cnt] = result
     return result
-    
 
 n = int(input())
 
 cost = [0] + list(map(int, input().split()))
-dp = [[-1]*(n+1) for _ in range(n+1)]
-print(make_it(1, 0))
+dp = [-1]*(n+1)
+print(make_it(n))
+
+# import sys
+# sys.setrecursionlimit(100000)
+# def make_it(index, cnt):
+
+#     if index > n: return 0
+
+#     if dp[index][cnt]>=0: return dp[index][cnt]
+
+#     result = make_it(index+1, cnt)
+#     count = cnt+index
+#     plus = cost[index]
+#     while count <= n:
+#         result = max(result, plus + make_it(index+1, count))
+#         count += index
+#         plus += cost[index]
+#     dp[index][cnt] = result
+#     return result
+    
+
+# n = int(input())
+
+# cost = [0] + list(map(int, input().split()))
+# dp = [[-1]*(n+1) for _ in range(n+1)]
+# print(make_it(1, 0))
