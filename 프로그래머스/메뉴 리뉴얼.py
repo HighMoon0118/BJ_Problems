c0d4efe61a9ba78f3a74a0a65adc2b33dbd53167
moon <mhi0118@naver.com>
2021-06-11 15:14:44 +0900

    메뉴리뉴얼

---
+++ 프로그래머스/메뉴 리뉴얼.py
@@ -0,0 +1,32 @@
+def solution(orders, course):
+    
+    ans = [{} for _ in range(len(course))]
+    words = [[s for s in word] for word in orders]
+    for word in words:
+        word = sorted(word)
+        tmp = makeIt("".join(word), 0, "", course)
+        
+        for s in tmp:
+            index = course.index(len(s))
+            ans[index][s] = ans[index].get(s, 0)+1
+    
+    answer = []     
+    for words in ans:
+        maxL = max(words.values()) if words.values() else 0
+        if maxL < 2: continue
+        for word, cnt in words.items():
+            if cnt == maxL:
+                answer.append(word)
+    answer.sort()
+    return answer
+
+def makeIt(word, index, tmp, course):
+    
+    if index == len(word):
+        if tmp and len(tmp) in course:
+            return [tmp]
+        return []
+    result = []
+    result += makeIt(word, index+1, tmp+word[index], course)
+    result += makeIt(word, index+1, tmp, course)
+    return result
