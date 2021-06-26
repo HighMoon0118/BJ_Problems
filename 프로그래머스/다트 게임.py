9c2d135a9dbe80d8e2846694577e22ed9f57b47d
moon <mhi0118@naver.com>
2021-06-27 08:09:42 +0900

    study

---
+++ 프로그래머스/다트 게임.py
@@ -0,0 +1,32 @@
+def solution(dartResult):
+    score = [0,0,0]
+    idx = 0
+    bonus = 0
+    i = 0
+    while i < len(dartResult):
+        
+        if dartResult[i] == '*':
+            score[idx-1] *= 2
+            if idx > 1:
+                score[idx-2] *= 2
+        elif dartResult[i] == "#":
+            score[idx-1] *= -1
+        elif dartResult[i] == "S":
+            idx += 1
+        elif dartResult[i] == "D":
+            score[idx] *= score[idx]
+            idx += 1
+        elif dartResult[i] == "T":
+            score[idx] *= score[idx]*score[idx]
+            idx += 1
+        elif dartResult[i] == "1" and dartResult[i+1] == "0":
+            score[idx] = 10
+            i += 1
+        else:
+            score[idx] = int(dartResult[i])
+        i += 1
+    
+    
+    return sum(score)
+
+print(solution("1D2S#10S"))
