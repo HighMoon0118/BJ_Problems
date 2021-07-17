089a5a4ab707bbc40dc32820baafc124b84d5cb3
moon <mhi0118@naver.com>
2021-07-17 23:28:19 +0900

    study

--- 스터디/BJ_1600.py
+++ 스터디/BJ_1600.py
@@ -1,30 +1,7 @@
 import sys
-sys.setrecursionlimit(1200000)
+from collections import deque
 
 input = sys.stdin.readline
-
-def makeIt(r, c, horse):
-
-    if r==n-1 and c==m-1:
-        return 0
-
-    if dp[r][c][horse]: return dp[r][c][horse]
-
-    dp[r][c][horse] = MAX
-    
-    for dr, dc in d:
-        nr, nc = r+dr, c+dc
-        if 0<=nr<n and 0<=nc<m and not board[nr][nc]:
-            dp[r][c][horse] = min(dp[r][c][horse], 1 + makeIt(nr, nc, horse))
-
-    if horse:
-        for dr, dc in d2:
-            nr, nc = r+dr, c+dc
-            if 0<=nr<n and 0<=nc<m and not board[nr][nc]:
-                dp[r][c][horse] = min(dp[r][c][horse], 1 + makeIt(nr, nc, horse-1))
-    
-    return dp[r][c][horse]
-
 
 d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 d2 = [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
@@ -32,11 +9,36 @@ d2 = [(1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1), (-2, 1), (-1, 2)]
 k = int(input())
 m, n = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-dp = [[[0]*(k+1) for _ in range(m)] for _ in range(n)]
 
-MAX = 400
+for r in range(n):
+    for c in range(m):
+        if board[r][c]:
+            board[r][c] = 31
+        else:
+            board[r][c] = -1
 
-ans = makeIt(0, 0, k)
+que = deque()
+que.append((0, 0, k, 0))
+board[0][0] = k
+isFinish = False
+
+while que:
+    r, c, h, cnt = que.popleft()
+    if r==n-1 and c==m-1: 
+        isFinish = True
+        break
+    for dr, dc in d:
+        nr, nc = r+dr, c+dc
+        if 0<=nr<n and 0<=nc<m and board[nr][nc] < h:
+            board[nr][nc] = h
+            que.append((nr, nc, h, cnt+1))
     
-if ans == MAX: print(-1)
-else: print(ans)
+    if h:
+        for dr, dc in d2:
+            nr, nc = r+dr, c+dc
+            if 0<=nr<n and 0<=nc<m and board[nr][nc] < h-1:
+                board[nr][nc] = h-1
+                que.append((nr, nc, h-1, cnt+1))
+
+if isFinish: print(cnt)
+else: print(-1)
