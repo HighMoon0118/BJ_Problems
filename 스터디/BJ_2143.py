fda39ecd9ecbd6c697b3fc4863724b19b4f73990
moon <mhi0118@naver.com>
2021-08-08 11:48:30 +0900

    study

---
+++ 스터디/BJ_2143.py
@@ -0,0 +1,40 @@
+import sys
+sys.setrecursionlimit(10000)
+
+input = sys.stdin.readline
+
+t = int(input())
+
+n = int(input())
+A = list(map(int, input().split()))
+
+m = int(input())
+B = list(map(int, input().split()))
+
+aDict = {}
+bDict = {}
+
+def makeIt(idx, mList, mDict, total, cnt, toggle):
+
+    if idx == len(mList) or toggle==2:
+        if cnt > 0:
+            mDict[total] = mDict.get(total, 0) + 1
+        return
+
+    if(toggle==0): 
+        makeIt(idx+1, mList, mDict, total+mList[idx], cnt+1, 1)
+        makeIt(idx+1, mList, mDict, total, cnt, 0)
+    elif(toggle==1): 
+        makeIt(idx+1, mList, mDict, total+mList[idx], cnt+1, 1)
+        makeIt(idx+1, mList, mDict, total, cnt, 2)
+
+makeIt(0, A, aDict, 0, 0, 0)
+makeIt(0, B, bDict, 0, 0, 0)
+
+ans = 0
+
+for num, cnt in aDict.items():
+    need = t-num
+    ans += cnt*bDict.get(need, 0)
+
+print(ans)
