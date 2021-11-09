87ebe59ae4fb4fcca83f004fc70d36a1b1ec7667
moon <mhi0118@naver.com>
2021-11-09 17:12:13 +0900

    study

--- 스터디/BJ_17070.py
+++ 스터디/BJ_17070.py
@@ -2,21 +2,29 @@ import sys
 
 input = sys.stdin.readline
 
-def dfs(row, col, how):
-    if row==n-1 and col==n-1:
+def dfs(how, row, col):
+    global ans
+
+    if row==n and col==n:
         return 1
+
+    if dp[how][row][col] >= 0: return dp[how][row][col]
+
     result = 0
     for pipe in next[how]:
         nr, nc = row+d[pipe][0], col+d[pipe][1]
-        if 0<=nr<n and 0<=nc<n and not board[nr][nc]:
-            if pipe!=2 or (not board[nr-1][nc] and not board[nr][nc-1]):
-                result += dfs(nr, nc, pipe)
-    return result
+        if not board[nr][nc] and (pipe!=2 or not (board[nr-1][nc] or board[nr][nc-1])):
+            result += dfs(pipe, nr, nc)
+    dp[how][row][col] = result
+
+    return dp[how][row][col]      
 
 n = int(input())
-board = [list(map(int, input().split())) for _ in range(n)]
+board = [[1]*(n+2)] + [[1]+list(map(int, input().split()))+[1] for _ in range(n)] + [[1]*(n+2)]
 
 next = ((0, 2), (1, 2), (0, 1, 2))
 d = ((0, 1), (1, 0), (1, 1))
 
-print(dfs(0, 1, 0))
+dp = [[[-1]*(n+2) for _ in range(n+2)] for _ in range(3)]
+
+print(dfs(0, 1, 2))
